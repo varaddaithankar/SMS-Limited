9aeb732b1b81d21c45a8231d01af744a836b12c8
varaddaithankar <73156321+varaddaithankar@users.noreply.github.com>
2022-11-02 01:07:53 +0530

    Sms

---
+++ src/Header.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <section className="container main-hero-container">
+          <div className="row">
+            <div className="col-12 col-lg-6 header-left-side d-flex justify-content-center flex-column align-items-start ">
+              <h1 className="display-2">
+                Online Payment Made <br /> Easy For You.
+              </h1>
+              <p className="main-hero-para">
+              Since the years of its existence, SMS has grown to become one of the largest infrastructure companies in Central India managing the work professionally even though it is closely held.
+              </p>
+              <h3>Get early access for you</h3>
+              <div className="input-group mt-3">
+                <input
+                  type="text"
+                  className="rounded-pill w-50  w-lg-75 me-3 p-2 form-control-text"
+                  placeholder="Enter Your Email"
+                />
+                <div className="input-group-button">Get it now</div>
+              </div>
+            </div>
+            {/*  --------------- main header right side--------------  */}
+            <div className="col-12 col-lg-6 header-right-side d-flex justify-content-center align-items-center main-herosection-images">
+              <img
+                src="./images/hero1.jfif"
+                alt="heroimg"
+                className="img-fluid"
+              />
+              <img
+                src="./images/hero4.jfif"
+                alt="heroimg4"
+                className="img-fluid main-hero-img2"
+              />
+            </div>
+          </div>
+        </section>
+      </header>
+    </>
+  );
+};
+
+export default Header;
